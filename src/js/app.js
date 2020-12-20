852b7f269032386d0dbd2f3bd181f567f70cc1c4
Arthur Geel <geelaj01@gmail.com>
2020-12-20 12:40:34 +0100

    Feature: Add Simple button (#1)
    * Update gitignore to exclude .DS_Store
    
    * Add simple button

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,24 @@
-console.log('hello world');
+console.log('App.js loaded');
+
+// const foo = document.getElementById('foo');
+// foo.addEventListener('click', function() {
+//   // console.log(this);
+//   toggleState(this);
+// })
+
+const buttonArray = document.querySelectorAll('.btn');
+for (let i = 0; i < buttonArray.length; i++) {
+  buttonArray[i].addEventListener('click', function() {
+    toggleState(this);
+  })
+}
+
+function toggleState(elem) {
+  if (elem.classList.contains('pressed')) {
+    elem.classList.remove('pressed');
+    console.log(`debug: powering off.`)
+  } else {
+    elem.classList.add('pressed');
+    console.log(`debug: ⚡️ powering on.`);
+  }
+}
