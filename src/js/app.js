adc79f0e4c0cb258c1c97158f7b19ab6b0555d87
Arthur Geel <geelaj01@gmail.com>
2020-12-21 10:38:38 +0100

    Add audio playback support

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,14 @@
 console.log('App.js loaded');
 
-// const foo = document.getElementById('foo');
-// foo.addEventListener('click', function() {
-//   // console.log(this);
-//   toggleState(this);
-// })
+
+/* Sound effects */
+btnClickOn = new sound('audio/btn-click-on.mp3', 1);
+btnClickOff = new sound('audio/btn-click-off.mp3', 1);
+poweringOn = new sound('audio/powering-on.mp3', 1);
 
 const buttonArray = document.querySelectorAll('.btn');
 for (let i = 0; i < buttonArray.length; i++) {
-  buttonArray[i].addEventListener('click', function() {
+  buttonArray[i].addEventListener('mousedown', function() {
     toggleState(this);
   })
 }
@@ -16,9 +16,33 @@ for (let i = 0; i < buttonArray.length; i++) {
 function toggleState(elem) {
   if (elem.classList.contains('pressed')) {
     elem.classList.remove('pressed');
-    console.log(`debug: powering off.`)
+    console.log(`debug: powering off.`);
+    btnClickOff.play();
   } else {
     elem.classList.add('pressed');
     console.log(`debug: ⚡️ powering on.`);
+    btnClickOn.play();
+    poweringOn.play();
+  }
+}
+
+function sound(src, volume) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  this.sound.volume = volume;
+  document.body.appendChild(this.sound);
+
+  this.play = function() {
+    // Reset
+    this.reset();
+    this.sound.play();
+  }
+
+  this.reset = function() {
+    this.sound.pause();
+    this.sound.currentTime = 0;
   }
 }
